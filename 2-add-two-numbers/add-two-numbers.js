04a892048095eb4c7b856a13c6ec08bec2838783
Michael Taklegiorgis <mikaeltek2015@gmail.com>
2025-05-11 15:09:56 +0300

    Time: 3 ms (57.28%) | Memory: 61.3 MB (66.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -1,19 +1,18 @@
 var addTwoNumbers = function(l1, l2) {
   if (l1 === null || l2 === null) return null
 
-  // using dummyHead can simplify linked list's calculation, dummyHead.next points to the new linked list
   let dummyHead = new ListNode(0)
   let cur1 = l1
   let cur2 = l2
-  let cur = dummyHead // cur is for the calculation in new linked list
-  let carry = 0 // carry-over symbol
+  let cur = dummyHead
+  let carry = 0
 
   while (cur1 !== null || cur2 !== null) {
     let val1 = cur1 !== null ? cur1.val : 0
     let val2 = cur2 !== null ? cur2.val : 0
     let sum = val1 + val2 + carry
-    let newNode = new ListNode(sum % 10) // the result of sum%10 ranges from 0 to 9, which is the value of the current digit
-    carry = sum >= 10 ? 1 : 0 // sum>=10, carry=1, so carry-over exists here
+    let newNode = new ListNode(sum % 10) 
+    carry = sum >= 10 ? 1 : 0 
     cur.next = newNode
     cur = cur.next
 
@@ -27,7 +26,7 @@ var addTwoNumbers = function(l1, l2) {
   }
 
   if (carry > 0) {
-    // If there's still carry-over in the end, then add a new node
+
     cur.next = new ListNode(carry)
   }
 
